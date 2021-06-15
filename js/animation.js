73948f623a0e84badadf875f5971abf474cece92
altf4m88 <fadhilmauladhani@gmail.com>
2021-06-15 20:36:43 +0700

    change some colors, fix svg, add nav information

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,9 @@
 const TL = new TimelineMax();
 
 const text1 = document.querySelector("#text-1");
-const text2 = document.querySelector("#text-2");
-const text3 = document.querySelector("#text-3");
-const dot = document.querySelector("#dot");
 
-TL
-.fromTo(text1, 1, {opacity: 0, x: -150}, {opacity: 1, x:0})
-.fromTo(text2, 1, {opacity: 0, x: -150}, {opacity: 1, x:0})
-.fromTo(text3, 1, {opacity: 0, x: -150}, {opacity: 1, x:0})
-.fromTo(dot, 1, {opacity: 0}, {opacity: 1})
+
+TL.delay(1)
+.fromTo(text1, 1, {opacity: 0}, {opacity: 1})
+
 
